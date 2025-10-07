bc2b65f7f22146e2d11219ae2f4f88c268ea682b
auramnar <auramnar@g.syr.edu>
2025-10-07 18:56:34 -0400

    kjj

--- 3-data/solutions/3-4-2.py
+++ 3-data/solutions/3-4-2.py
@@ -2,3 +2,10 @@ import streamlit as st
 import pandas as pd
 from check_functions import clean_currency, detect_whale, detect_tipper
 
+from check_functions import clean_currency, detect_whale, detect_tipper
+
+st.title("Dining Check Data")
+
+#load
+checks = pd.read_csv('https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/dining/check-data.csv')
+
