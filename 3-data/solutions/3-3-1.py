7e9ccc4c8347d547a915bd285a27a6d69c88aab8
auramnar <auramnar@g.syr.edu>
2025-10-07 18:26:20 -0400

    edits

--- 3-data/solutions/3-3-1.py
+++ 3-data/solutions/3-3-1.py
@@ -1,3 +1,12 @@
 import pandas as pd
+import numpy as np
 import requests
+import streamlit as st
+
+
+link ="https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/json-samples/employees-dict.json"
+response = requests.get(link)
+employees = response.json()
+st.write(employees) # this will show the dictionary
+
 
