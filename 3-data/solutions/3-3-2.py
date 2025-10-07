b318874dc4583273937e4b3f4e905b40a0a8c11b
auramnar <auramnar@g.syr.edu>
2025-10-07 18:43:39 -0400

    brief mods

--- 3-data/solutions/3-3-2.py
+++ 3-data/solutions/3-3-2.py
@@ -4,6 +4,10 @@ import requests
 import numpy as np
 
 
+import streamlit as st
+import pandas as pd
 
+base = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/minimart/"
+months = ['jan', 'feb', 'mar', 'apr']
 
-
+st.title("Who's not Buying from MiniMart?")
