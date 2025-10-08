449655a0663fb41a505695989677a555191a64e6
auramnar <auramnar@g.syr.edu>
2025-10-08 08:00:43 -0400

    edits

--- 3-data/solutions/3-4-1.py
+++ 3-data/solutions/3-4-1.py
@@ -6,13 +6,18 @@ from check_functions import clean_currency
 
 st.title("Dining Check Data")
 
-# load
+# load the checks dataset
 checks = pd.read_csv('https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/dining/check-data.csv')
 
 # transformations
+#clean the 'total amount of check' and 'gratuity' columns
 checks['total_amount_of_check_cleaned'] = ???
 checks['gratuity_cleaned'] = ????
 
+#checkpoint
+#st.dataframe(checks)
+
+#calculate 
 checks['price_per_item'] = ???
 checks['price_per_person'] = ???
 checks['items_per_person'] = ???
@@ -20,7 +25,7 @@ checks['tip_percentage'] = ???
 
 st.dataframe(checks, width=1000)
 
-# display
+# display the dataframe
 col1,col2,col3,col4 = st.columns(4)
 
 col1.metric("Average Price Per Item", checks['price_per_item'].mean())
@@ -28,4 +33,5 @@ col2.metric("Average Price Per Person", checks['price_per_person'].mean())
 col3.metric("Average Items Per Person", checks['items_per_person'].mean())
 col4.metric("Average Tip Pct", checks['tip_percentage'].mean())
 
+# describe the dataframe
 st.dataframe(checks.describe())
