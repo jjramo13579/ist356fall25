571ca40ba64ea40914c920cba6abd99d6496e51e
auramnar <auramnar@g.syr.edu>
2025-10-22 08:24:44 -0400

    api

--- 4-api/solutions/4-1-1.py
+++ 4-api/solutions/4-1-1.py
@@ -1,7 +1,12 @@
 import requests
 import pandas as pd
+import numpy as np
+import json
 import streamlit as st
 
 st.title('API Example API to Pandas')
 
 
+uri = "https://jsonplaceholder.typicode.com/users/"
+
+# Write your code here $
--- 4-api/solutions/4-1-2.py
+++ 4-api/solutions/4-1-2.py
@@ -1,3 +1,18 @@
+'''
+Use the IoT portal for the URI to search for funny names. Once you understand how to invoke the REST API, write a streamlit to 
+input a name and return the matches in a dataframe. 
+
+curl -X 'GET' \
+  'https://cent.ischool-iot.net/api/funnyname/search?q=an' \
+  -H 'accept: application/json'
+
+
+'''
+
+
+
+
+
 import requests
 import pandas as pd
 import streamlit as st
@@ -5,4 +20,8 @@ import streamlit as st
 
 st.title("Funny Name Search")
 
-uri = "https://cent.ischool-iot.net/api/funnyname/search"
+url = "https://cent.ischool-iot.net/api/funnyname/search"
+search = st.text_input("Enter a name to search for funny names:")
+if st.button("Search"):
+    # Complete the code here to call the API and display results in a dataframe
+    
